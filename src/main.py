51c60acef979f21852eb8ff49bcc949b652e9e33
Runzhi Xing <xrunzhi@amazon.com>
2022-01-23 17:03:16 -0800

    Add core logic to execute rsync command

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import json
 import click
 import os
+import subprocess
+import random
+import string
 
 
 @click.command()
@@ -10,7 +13,7 @@ def main(setup):
         click.echo('Setting up remote host locations')
     else:
         click.echo('Syncing local files to remote....')
-        path = '/Users/xrunzhi/.config/ninja-dev-sync.json'
+        path = '/Users/xrunzhi/.config/remote-dev-sync.json'
         with open(path, 'r') as config_file:
             settings = json.load(config_file)
         remote_host = settings['DefaultHost']
@@ -20,9 +23,37 @@ def main(setup):
 
 def sync(workspace, remote_host):
     local_dir = workspace['LocalSync']
-    remote_dir = workspace['RemoteHost']
-    print(local_dir)
-    print(remote_dir)
+    remote_host = workspace['RemoteHost']
+    target_dir = workspace['RemoteSync']
+    flags = []
+    ssh_flags = [
+        "-o", "PasswordAuthentication=no",
+        "-o", "ServerAliveInterval=5",
+        "-o", "ServerAliveCountMax=2",
+        "-o", "ConnectTimeout=10",
+        "-o", "ControlMaster=auto",
+        "-o", "ControlPersist=30m",
+        "-o", "ControlPath=" + get_control_path() + "",
+    ]
+    ssh_command = ' '.join(['ssh', *ssh_flags])
+    rsync(local_dir, remote_host, target_dir, ssh_command, flags)
+
+
+def get_control_path():
+    user_home_dir = os.getenv('HOME')
+    letters = string.ascii_lowercase
+    random_string = ''.join(random.choice(letters) for i in range(4))
+    return user_home_dir + '/.ssh/remote-dev-sync_' + random_string
+
+
+def rsync(from_host, to_host, target_dir, ssh_command, flags):
+    if from_host[-1] != '/':
+        from_host += '/'
+    target = f'{to_host}:{target_dir}'
+    args = ['rsync', '-e', ssh_command, '-rlptzv', '--delete-after', *flags, from_host, target]
+    command = subprocess.run(args)
+    print(f'rsync [...] {from_host} {target}')
+
 
 if __name__ == '__main__':
     main()
